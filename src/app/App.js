b696070ac369d784e2206a21a397915cda9ba51a
rsubdy <subdysiak@gmail.com>
2023-10-19 19:49:55 +0200

    Initial setup

---
+++ src/app/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+    </div>
+  );
+}
+
+export default App;
