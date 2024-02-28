a45636708281098c5a8ba4bd3d61516e85a4494f
rsubdy <subdysiak@gmail.com>
2024-02-28 22:57:19 +0100

    Slight changes to hello

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -17,8 +17,8 @@ const toggleHello = () => {
       <Collapse in={helloVisibility}>
         <div id="hello-collapse" aria-expanded={helloVisibility}>
           <h4 class="text-black">Full fridge seems more troubling than an empty one?</h4>
-          <p class="text-black">This app will help you find a perfect recipe.
-            Just give me your preferences, show what's in your fridge and I've got you covered!
+          <p class="text-black">
+            First declare your preferences, then show what's in your fridge and this app will find you a recipe!
           </p>
           <Button variant="btn btn-light border border-black border-3 fw-bold" aria-controls="hello-collapse diet-expand" onClick={()=>{toggleHello()}}>
             Let's start!
