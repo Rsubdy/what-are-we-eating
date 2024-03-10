d7a3460c3f8f8f41cecf7a233993f8de5e051044
rsubdy <subdysiak@gmail.com>
2024-03-10 22:36:53 +0100

    Added styling and image on landing page

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import DietSelection from '../features/diets/DietSelection';
-import { Container, Button, Collapse } from 'react-bootstrap';
-
+import { Container, Button, Collapse, Image, Row, Col } from 'react-bootstrap';
+import fridgeStocked from '../img/fridgeStocked.png'
 function Hello() {
   
 const [helloVisibility, setHelloVisibility] = useState(true);
@@ -16,13 +16,24 @@ const toggleHello = () => {
     <Container className="d-grid justify-content-center">
       <Collapse in={helloVisibility}>
         <div id="hello-collapse" aria-expanded={helloVisibility}>
+          <Container>
+          <Row>
           <h4 className="text-black">Full fridge seems more troubling than an empty one?</h4>
-          <p className="text-black">
-            First declare your preferences, then show what's in your fridge and this app will find you a recipe!
-          </p>
+          </Row>
+          <Row>
+            <Col className="col-6"> 
+              <p className="text-black">First declare your preferences, then show what's in your fridge and this app will find you a recipe!</p>
+            </Col>
+            <Col className="col-6">
+              <Image className="border border-black border-4 m-6" src={fridgeStocked} alt="Stocked fridge" width="100%" height="100%" />
+            </Col>
+          </Row>
+          
           <Button variant="btn btn-light border border-black border-3 fw-bold" aria-controls="hello-collapse diet-expand" onClick={()=>{toggleHello()}}>
             Let's start!
           </Button>
+          
+          </Container>
         </div>
       </Collapse>
       <Collapse in={dietVisibility}>
